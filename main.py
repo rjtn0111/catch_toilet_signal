5346266723a66b62707bd9647d9e3f6caee586c3
rjtn0111 <rj.tn0111@gmail.com>
2024-11-13 22:27:14 +0900

    change bbox coord

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # やること
+# 色変化中の安定化
 # 接続切れの対策
 # スプレットシートへの書き込み機能
 # 閾値などのハイパラ調整
@@ -6,10 +7,10 @@
 from color_detection import detect_color_change
 
 # YouTubeの動画URL
-url = 'https://www.youtube.com/live/bPh0zpkiu74?si=kK423f38YP3F_gGD'
+url = 'https://www.youtube.com/live/bPh0zpkiu74?si=iypxldiINNlMpaRo'
 
 # 監視エリアの位置とサイズ（例：左上の位置、50x50ピクセル）
-monitor_area = (950, 800, 100, 50)  # (x, y, width, height)
+monitor_area = (950, 700, 100, 50)  # (x, y, width, height)
 
 # detect_color_change関数を呼び出す
 detect_color_change(
@@ -17,5 +18,5 @@ detect_color_change(
     monitor_area=monitor_area,
     color_change_threshold=50,
     min_change_for_notification=20,
-    show=True
+    show=False
 )
